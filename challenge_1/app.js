278f8cc8c8dfc0212dfa82b3bdb19675394d9658
Bryce Balbon <bbalbon@gmail.com>
2020-01-13 10:44:25 -0800

    Add basic outline

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,13 @@
+let square = document.getElementsByClassName('square');
+console.log(square);
 
+square.addEventListener('click', () => {
+    console.log('hit')
+})
+
+const placePiece = () => {
+    square.textContent = 'x';
+    let element = document.getElementById(this);
+    console.log(element);
+}
 

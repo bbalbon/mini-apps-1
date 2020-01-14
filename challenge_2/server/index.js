00abde32511fbce817981f69befd6161e6f9a0fc
bbalbon <bbalbon@gmail.com>
2020-01-13 18:02:16 -0800

    Add CSS files and form input

--- challenge_2/server/index.js
+++ challenge_2/server/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname + '/../client'));
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-
-app.use(express.static(__dirname + '/../client'))
 
 app.listen(port, () => {
   console.log(`Chicken App listening on port ${port}!`);

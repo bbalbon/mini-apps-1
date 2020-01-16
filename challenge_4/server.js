554b034f3eb7c368b18e6b6877aebf362e09c153
bbalbon <bbalbon@gmail.com>
2020-01-16 09:29:42 -0800

    Add pck.json

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
 
+app.use(express.static(__dirname + '/./public'));
+app.use(parser.json());
+
+app.get('/', (req, res) => {
+  res.send('Yes');
+})
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000!');
+})
 

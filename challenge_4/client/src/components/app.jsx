a8becd12420c4b7842ae79a5a85363b66517e482
bbalbon <bbalbon@gmail.com>
2020-01-16 13:54:05 -0800

    Remove unnecessary file

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      board: [[0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0]]
+    }
+  }
+
+  updateBoard () {
+  }
+
   render () {
+
     return (
-      <div>
-        Hello JimBob
+      <div id="board">
+        {this.state.board.map((item, i) => (
+          
+        ))}
       </div>
     );
   }
---
+++ challenge_4/client/src/components/square.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      toggled: false
+    }
+  }
+
+  toggleColor() {
+    
+  }
+
+
+}
+
+
+
+export default Square;
